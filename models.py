f5da7fd46fbbe768696e20dd82b3cd2bd9064b2d
Leo-dev-gif <leonardo.stefani.014@gmail.com>
2025-01-09 13:29:39 +0100

    Create models.py

---
+++ models.py
@@ -0,0 +1,8 @@
+from app import db
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    category = db.Column(db.String(100), nullable=False)
